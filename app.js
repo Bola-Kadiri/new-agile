60eb87fe14108471b8538fed6c82a76497813a12
Bola-Kadiri <bolajimoh25@gmail.com>
2022-07-05 09:45:23 +0100

    new project

---
+++ app.js
@@ -0,0 +1,8 @@
+let transfer = document.querySelector('.transfer')
+let transferContent = document.querySelector('.transfer-content')
+
+
+
+transfer.addEventListener('click', ()=>{
+    transferContent.style.display ='flex'
+})
